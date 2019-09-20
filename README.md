2dceb8e80b03490c339e110548dd5f6d47f44d56
fdstypical <networkdaemonyung@gmail.com>
2019-09-20 19:22:46 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
- A simple ReactJs application for calculating daily expenses and viewing statistics of expenses for all time! 
- Try here: https://fdstypical.github.io/greedy-money/
---
+++ precache-manifest.77787e59608bb8c727b405336f9e4077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7021f45aec73526411d231b214a08a2",
+    "url": "/greedy-money/index.html"
+  },
+  {
+    "revision": "cee1653286223c6776f7",
+    "url": "/greedy-money/static/css/main.89870b82.chunk.css"
+  },
+  {
+    "revision": "8dd7d5d72d5352b0e91e",
+    "url": "/greedy-money/static/js/2.7be870b8.chunk.js"
+  },
+  {
+    "revision": "cee1653286223c6776f7",
+    "url": "/greedy-money/static/js/main.897f0b95.chunk.js"
+  },
+  {
+    "revision": "044a75097ba83b22291b",
+    "url": "/greedy-money/static/js/runtime~main.1dbcccfa.js"
+  }
+]);
